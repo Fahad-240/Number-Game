b278d5dfdffeba376e0815bfb83f8bb6b6bdf38f
Fahad Ahmed <159483429+Fahad-240@users.noreply.github.com>
2024-09-22 01:32:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let guessNumber = prompt ("Guess the Number 1 to 10")
+let secretNumber = Number(3)
+if(guessNumber == secretNumber){
+    alert("Bingo! Correct Answer")
+}else  {
+    alert("Wrong Answer. The Secret Number Was 3 ")
+}
